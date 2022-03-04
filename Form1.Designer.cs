af880b1a6917fa88bd114b1eb047d9502a07b7a7
KamishiroIyamoto <55036694+KamishiroIyamoto@users.noreply.github.com>
2022-03-04 23:43:14 +0500

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace SpeechToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        [System.Obsolete]
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.record = new System.Windows.Forms.Button();
+            this.recognize = new System.Windows.Forms.Button();
+            this.read = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.write = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // record
+            // 
+            this.record.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.record.Location = new System.Drawing.Point(7, 7);
+            this.record.Margin = new System.Windows.Forms.Padding(7);
+            this.record.Name = "record";
+            this.record.Size = new System.Drawing.Size(200, 55);
+            this.record.TabIndex = 0;
+            this.record.Text = "Запись";
+            this.record.UseVisualStyleBackColor = true;
+            this.record.Click += new System.EventHandler(this.record_Click);
+            // 
+            // recognize
+            // 
+            this.recognize.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.recognize.Enabled = false;
+            this.recognize.Location = new System.Drawing.Point(7, 76);
+            this.recognize.Margin = new System.Windows.Forms.Padding(7);
+            this.recognize.Name = "recognize";
+            this.recognize.Size = new System.Drawing.Size(200, 55);
+            this.recognize.TabIndex = 3;
+            this.recognize.Text = "Распознать";
+            this.recognize.UseVisualStyleBackColor = true;
+            this.recognize.Click += new System.EventHandler(this.recognize_Click);
+            // 
+            // read
+            // 
+            this.read.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.read.Enabled = false;
+            this.read.Location = new System.Drawing.Point(7, 76);
+            this.read.Margin = new System.Windows.Forms.Padding(7);
+            this.read.Name = "read";
+            this.read.Size = new System.Drawing.Size(200, 55);
+            this.read.TabIndex = 4;
+            this.read.Text = "Прочитать";
+            this.read.UseVisualStyleBackColor = true;
+            this.read.Click += new System.EventHandler(this.read_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Controls.Add(this.record);
+            this.flowLayoutPanel1.Controls.Add(this.recognize);
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.flowLayoutPanel1.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(217, 145);
+            this.flowLayoutPanel1.TabIndex = 6;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.Controls.Add(this.textBox1);
+            this.flowLayoutPanel2.Controls.Add(this.read);
+            this.flowLayoutPanel2.Controls.Add(this.write);
+            this.flowLayoutPanel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.flowLayoutPanel2.FlowDirection = System.Windows.Forms.FlowDirection.TopDown;
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(0, 145);
+            this.flowLayoutPanel2.Margin = new System.Windows.Forms.Padding(0);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(217, 210);
+            this.flowLayoutPanel2.TabIndex = 7;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.textBox1.Location = new System.Drawing.Point(7, 7);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(7);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(200, 55);
+            this.textBox1.TabIndex = 8;
+            // 
+            // write
+            // 
+            this.write.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.write.Enabled = false;
+            this.write.Location = new System.Drawing.Point(7, 145);
+            this.write.Margin = new System.Windows.Forms.Padding(7);
+            this.write.Name = "write";
+            this.write.Size = new System.Drawing.Size(200, 55);
+            this.write.TabIndex = 9;
+            this.write.Text = "Записать";
+            this.write.UseVisualStyleBackColor = true;
+            this.write.Click += new System.EventHandler(this.write_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(14F, 31F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(217, 358);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Font = new System.Drawing.Font("POE Vetica", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(7, 8, 7, 8);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "NeoSynthizer";
+            this.flowLayoutPanel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.flowLayoutPanel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button record;
+        private System.Windows.Forms.Button recognize;
+        private System.Windows.Forms.Button read;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button write;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Windows.Forms;
+using NAudio.Wave;
+using System.Net;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Speech.Synthesis;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace SpeechToText
+{
+    public partial class Form1 : Form
+    {
+        WaveIn waveIn;
+        WaveFileWriter writer;
+        readonly string outputFilename = "demo.wav";
+        bool ON = false;
+        string language = "ru-RU";
+        [Obsolete]
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        [Obsolete]
+        private void record_Click(object sender, EventArgs e)
+        {
+            read.Enabled = false;
+            write.Enabled = false;
+            if (ON == false)
+            {
+                textBox1.Text = "Запись...";
+                waveIn = new WaveIn();
+                waveIn.DeviceNumber = 0;
+                waveIn.DataAvailable += waveIn_DataAvailable;
+                waveIn.RecordingStopped += new EventHandler<StoppedEventArgs>(waveIn_RecordingStopped);
+                waveIn.WaveFormat = new WaveFormat(16000, 1);
+                writer = new WaveFileWriter(outputFilename, waveIn.WaveFormat);
+                record.Text = "Стоп";
+                waveIn.StartRecording();
+                ON = true;
+                recognize.Enabled = false;
+            }
+            else
+            {
+                textBox1.Text = "Конец записи";
+                waveIn.StopRecording();
+                ON = false;
+                record.Text = "Запись";
+                recognize.Enabled = true;
+            }
+        }
+        [Obsolete]
+        void waveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            writer.WriteData(e.Buffer, 0, e.BytesRecorded);
+        }
+        void waveIn_RecordingStopped(object sender, EventArgs e)
+        {
+            waveIn.Dispose();
+            waveIn = null;
+            writer.Close();
+            writer = null;
+        }
+        private bool keyword_processing(string result)
+        {
+            switch (result.ToLower().Split()[0])
+            {
+                case "язык":
+                case "language":
+                    switch (result.ToLower().Split()[1])
+                    {
+                        case "английский":
+                            language = "en-US";
+                            textBox1.Text = "Language is set to English";
+                            break;
+                        case "russian":
+                            language = "ru-RU";
+                            textBox1.Text = "Язык установлен на Русский";
+                            break;
+                        default:
+                            language = "ru-RU";
+                            break;
+                    }
+                    break;
+                case "выход":
+                case "exit":
+                    Close();
+                    break;
+                case "интернет":
+                case "internet":
+                    Process.Start("https://www.google.ru");
+                    textBox1.Text = "Google";
+                    break;
+                case "автор":
+                case "author":
+                    Process.Start("https://vk.com/msneron");
+                    textBox1.Text = "VK: msneron";
+                    break;
+                case "найти":
+                case "search":
+                    string t = "";
+                    foreach(var word in result.ToLower().Split())
+                        t += word + "+";
+                    t = t.Substring(result.ToLower().Split()[0].Length + 1);
+                    t = t.Remove(t.Length - 1, 1);
+                    Process.Start($"https://www.google.com/search?q={t}");
+                    textBox1.Text = "Google";
+                    break;
+                default:
+                    return true;
+            }
+            return false;
+        }
+        private void recognize_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "Распознавание...";
+            WebRequest request = WebRequest.Create($"https://www.google.com/speech-api/v2/recognize?output=json&lang={language}&key=AIzaSyBOti4mM-6x9WDnZIjIeyEU21OpBXqWBgw");
+            request.Method = "POST";
+            byte[] byteArray = File.ReadAllBytes(outputFilename);
+            request.ContentType = "audio/l16; rate=16000";
+            request.ContentLength = byteArray.Length;
+            request.GetRequestStream().Write(byteArray, 0, byteArray.Length);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string strtrs = reader.ReadToEnd();
+            var rg = new Regex(@"transcript" + '"' + ":" + '"' + "([A-Z, А-Я, a-z,а-я, ,0-9]*)");
+            var result = rg.Match(strtrs).Groups[1].Value;
+            if (keyword_processing(result))
+            {
+                textBox1.Text = result;
+                read.Enabled = true;
+                write.Enabled = true;
+            }
+            reader.Close();
+            response.Close();
+        }
+        private async void read_Click(object sender, EventArgs e)
+        {
+            var synth = new SpeechSynthesizer();
+            synth.SetOutputToDefaultAudioDevice();
+            await Task.Run(() => synth.Speak(textBox1.Text));
+        }
+
+        private void write_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (StreamWriter sw = new StreamWriter("text.txt", false, System.Text.Encoding.Default))
+                {
+                    sw.Write(textBox1.Text);
+                }
+                textBox1.Text = "Запись выполнена";
+                write.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                textBox1.Text = ex.Message;
+            }
+        }
+    }
+}
